8bf263befbf705c2bec717f359de5d29796dc545
Loc Pham <phamducloc2003@gmail.com>
2025-05-21 01:08:08 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+print("Getting images for display")
+
+# for i in range(16):
+#     plt.subplot(4, 4, i + 1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.imshow(training_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[training_labels[i][0]])
+
+# plt.show()
+
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+model = models.Sequential() 
